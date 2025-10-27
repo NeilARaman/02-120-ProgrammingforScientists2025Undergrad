5e2073c18e0584511c3248f8b571fca8d7e77861
Neil Aditya Raman <neilr@andrew.cmu.edu>
2025-10-27 19:41:59 -0400

    Update main.py

--- Starter Code/upgma/main.py
+++ Starter Code/upgma/main.py
@@ -1,6 +1,8 @@
 from functions import upgma
 from io_util import read_matrix_from_file, write_newick_to_file
-
+write_newick_to_file
 
 def main() -> None:
     print("Happy trees.")
+
+    v = Node(num)
